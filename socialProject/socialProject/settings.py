0ad18c7f818dbe5e8e4056d6584d90a36300c9d8
xueyang0312 <jeak831130015@gmail.com>
2020-05-31 15:17:21 +0800

    init base.html

--- socialProject/socialProject/settings.py
+++ socialProject/socialProject/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'login.apps.LoginConfig',
 
+
     # all auth
     'allauth',
     'allauth.account',
--- socialProject/socialProject/urls.py
+++ socialProject/socialProject/urls.py
@@ -19,6 +19,6 @@ from login import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.home, name="home"),
+    path('', views.login, name="login"),
     path('accounts/', include('allauth.urls')),
 ]
