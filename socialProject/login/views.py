467c2583856898e91e722a83b2d18c216b0a18f0
xueyang0312 <jeak831130015@gmail.com>
2020-06-09 22:11:59 +0800

    rewrite base.html

--- socialProject/login/views.py
+++ socialProject/login/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.contrib import auth
 from django.contrib.auth.models import User
 from django.contrib.auth.decorators import login_required
+from django.http import Http404
 
 # Create your views here.
 def login(request):
